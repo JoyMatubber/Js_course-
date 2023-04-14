d80a2091eb1ba39fd326fc68b9fa0a82ab8c5084
JoyMatubber <joy.matubber.41959@gmail.com>
2023-04-15 00:29:50 +0600

    Some and every method with request Loan done

--- Array/BankProject.js
+++ Array/BankProject.js
@@ -214,6 +214,15 @@ btnClose.addEventListener('click', function (e) {
   inputCloseUsername.value = inputClosePin.value = ''; // set this positin is emplty
   // console.log('delete');
 });
+btnLoan.addEventListener('click', function (e) {
+  e.preventDefault();
+  const amount = Number(inputLoanAmount.value);
+  if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
+    currentAccount.movements.push(amount);
+    UpdateUI(currentAccount);
+  }
+  inputLoanAmount.value = '';
+});
 
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
